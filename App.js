2de716f7a532704244ad112ec4821a4f375f529c
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-08-03 12:05:54 -0300

    AsyncStorage + diseño

--- App.js
+++ App.js
@@ -1,25 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View, Button, TextInput, TouchableOpacity, AsyncStorage } from 'react-native';
-import React from 'react';
+import { StatusBar } from 'expo-status-bar'
+import { ImageBackground, FlatList, StyleSheet, Text, View, Button, TextInput, TouchableOpacity } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { useState } from 'react'
+import { Input } from 'react-native-elements'
+const bg1 = {uri: 'https://marketplace.canva.com/EAE7VlC9ueE/1/0/900w/canva-fondo-de-pantalla-para-celular-floral-rosa-pastel-_cO75HW94eI.jpg'}
+const bg2 = {uri: 'https://image.winudf.com/v2/image1/aW8ud2FsbHBhcGVyLnBhcGVsLnBhcmVkZV9zY3JlZW5fMF8xNjc2NzM5MjkwXzA3Mg/screen-0.webp?fakeurl=1&type=.webp'}
 
 export default function App() {
-  const [text, onChangeText] = React.useState('')
-  const [texto, onTexto] = React.useState([])
-  
-  
+  const [text, onChangeText] = useState('')
+  const [texto, onTexto] = useState([])
+  const [background, setBackground] = useState(bg1)
+  const [listaTareas, setListaTareas] = useState([])
+
+  const guardarTarea = async(tarea = "hay q cambiar esto x la tarea a agregar") => {
+    try {
+      const tareas = [...JSON.parse(await AsyncStorage.getItem('tareas'))]
+      tareas.push(tarea)
+      await AsyncStorage.setItem('@tareas', JSON.stringify(tareas))
+      setListaTareas(tareas)
+    } catch (err) {
+      throw new Error('ERROR EN: guardarTarea')
+    }
+  }
+  const eliminarTarea = async(tareaIndex) => {
+    try {
+      const tareas = [...JSON.parse(await AsyncStorage.getItem('tareas'))]
+      tareas = tareas.filter((tarea, index) => index !== tareaIndex)
+      await AsyncStorage.setItem('@tareas', JSON.stringify(tareas))
+      setListaTareas(tareas)
+    } catch (err) {
+      throw new Error('ERROR EN: eliminarTarea')
+    }
+  }
+  // const obtenerTareas = async () => {
+  //   try {
+  //     return [...JSON.parse(await AsyncStorage.getItem('tareas'))];
+  //   } catch (error) {
+  //     console.log(err)
+  //     throw new Error('fallo leve')
+  //   }
+  // }
+
+  const handleBackground = () => setBackground(bg => bg === bg1 ? bg2 : bg1)
+  const handlePress = () => {
+    
+  }
 
     return (
+      <ImageBackground source={background} resizeMode="cover" style={styles.background}>
     <View style={styles.container}>
-      <TextInput
+      
+      <Text style={styles.titulo}>TO DO LIST</Text>
+      <Input
         onChangeText={onChangeText}
         value={text}
-        placeholder="Ingresar texto"
+        placeholder="Ingresar tarea"
       />
       <View style={styles.posicionCentrado}>
-        <TouchableOpacity style={styles.botonAgregar} >
+        <TouchableOpacity style={styles.botonAgregar} onPress={handlePress}>
           <Text style={[{ fontSize: '1rem', fontWeight: "bold", }]}>Agregar tarea</Text>
         </TouchableOpacity>
       </View>
+      
 
       <FlatList
         data={[
@@ -28,7 +70,14 @@ export default function App() {
         renderItem={({ item }) => <Text style={styles.item}>{item.key}</Text>}
       />
       <Text>{texto}</Text>
+
+      <View style={styles.posicionCentrado}>
+        <TouchableOpacity style={styles.botonAgregar} onPress={handleBackground}>
+          <Text style={[{ fontSize: '1rem', fontWeight: "bold", }]}>Cambiar fondo</Text>
+        </TouchableOpacity>
+      </View>
     </View>
+    </ImageBackground>
   );
 }
 
@@ -43,6 +92,15 @@ const styles = StyleSheet.create({
     fontSize: 18,
     height: 44,
   },
+  titulo: {
+    display: 'flex',
+    justifyContent: 'center',
+    fontSize: '1.5rem',
+    marginTop: '3rem',
+    marginBottom: '2rem',
+    fontWeight: '700',
+    fontSize: 50,
+  },
   posicionCentrado:{
     display: 'flex',
     alignItems: 'center'
@@ -56,5 +114,9 @@ const styles = StyleSheet.create({
     maxHeight: '3rem',
     minWidth: '10rem',
     padding: 10,
+    margin: '1rem'
+  },
+  background: {
+    width: '100%'
   }
 });
